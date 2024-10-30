e56e4b9fd082e6be76c9663bc5e86911610ad19e
Guo Zhi Jun <41243133@nfu.edu.tw>
2024-10-30 19:29:07 +0800

    Update array.cpp

--- hw1030/src/array.cpp
+++ hw1030/src/array.cpp
@@ -1,43 +1,48 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int *change_size(int *&arr, int size, int new_size) {
-    int *new_arr = new int[new_size]; //新的陣列 
-    int *recycler = arr; //舊的陣列 
-    copy(arr, arr + size, new_arr); //舊的複製到新的 
-    int num=size; //舊陣列大小
- 
-    while(num<new_size){
-    	num*=2; //當舊陣列大小小於新陣列大小，新陣列長度=舊陣列長度*2 size*=2 
-	}
-	
-	for(int i=size-1;i<num;i++){
-		new_arr[i]=i+1;   //把新陣列中的空陣列塞入數字 
-	}
-    delete[] recycler; //刪除舊陣列 
-    arr = new_arr;  
+
+int *change_size(int *&arr, int &size, int new_size) {
+    int *new_arr = new int[new_size]; // 建立新陣列
+    int *recycler = arr; // 指向舊陣列以便後續刪除
+
+    // 將舊陣列的元素複製到新陣列
+    copy(arr, arr + size, new_arr);
+
+    // 將新位置的元素填入值
+    for (int i = size; i < new_size; i++) {
+        new_arr[i] = i + 1;  // 這裡可以依照需求填入其他數值
+    }
+
+    delete[] recycler; // 釋放舊陣列的記憶體
+    arr = new_arr;  // 將新陣列指向 arr
+    size = new_size;  // 更新 size 的值為 new_size
     return arr;
 }
-void print_arr(int *arr,int size){   //輸出陣列函式 
-	for(int i=0;i<size;i++){
-		cout<<arr[i]<<" ";
-	}
-	cout<<endl;
-} 
+
+void print_arr(int *arr, int size) { // 印出陣列函數
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
 int main() {
     int size = 3;
     int *arr = new int[size];
 
-    // Inserting elements.
+    // 初始化陣列元素
     for (int i = 0; i < size; i++) {
         arr[i] = i + 1;
-    };
+    }
 
-    print_arr(arr, size);  // Print initial array.
-	
+    print_arr(arr, size);  // 輸出初始陣列
+
     int new_size;
-    
-	cout<<"輸入 new_size"<<endl;
-	cin>>new_size;  //輸入新陣列大小 
-    change_size(arr, size, new_size); // 
-    print_arr(arr, new_size);  // Size has changed, print the new array.
+    cout << "輸入 new_size: ";
+    cin >> new_size;  // 輸入新陣列大小
+
+    change_size(arr, size, new_size);  // 改變陣列大小
+    print_arr(arr, new_size);  // 輸出新的陣列
+
+    delete[] arr;  // 最後釋放記憶體
 }
